6d7cf88e7d39da76a90e2217739f8cdac719c149
Ömer Yazar <omeryazar0808@gmail.com>
2024-08-09 16:43:33 +0300

    feat(frontend): add register page ui

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,3 +1,17 @@
+import { CardWrapper } from "../components/auth/CardWrapper";
+import { RegisterForm } from "../components/auth/RegisterForm";
+
 export default function RegisterPage() {
-  return <div>register page</div>;
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <CardWrapper
+        headerTitle="Welcome"
+        headerSubtitle="Please enter your details to sign up"
+        footerLabel="Already have an account?"
+        footerAction={{ label: "Sign in to your account", href: "/login" }}
+      >
+        <RegisterForm />
+      </CardWrapper>
+    </div>
+  );
 }
