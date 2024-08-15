959066cb98f4067af06379466e5fc917b9aefdba
Ömer Yazar <omeryazar0808@gmail.com>
2024-08-15 16:04:02 +0300

    fix: fixed app file

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <button>
-        <Link to="/login">Login</Link>
-      </button>
-      <button>
-        <Link to="/register">Register</Link>
-      </button>
-    </>
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="flex flex-col gap-2">
+        <button>
+          <Link to="/login">Login</Link>
+        </button>
+        <button>
+          <Link to="/register">Register</Link>
+        </button>
+      </div>
+    </div>
   );
 }
 
